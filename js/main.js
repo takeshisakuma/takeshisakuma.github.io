663ff5153615eb6330b2876d3a587adf0e1e0187
takeshisakuma <saude0518@gmail.com>
2019-11-11 07:26:15 +0900

    add work

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ const loadedTransiton = () => {
   loaderLayer.classList.remove("active");
 }
 window.addEventListener("load", loadedTransiton);
+
+
+
+
+/*
+const aaa = document.getElementsByClassName("favorite__item");
+console.log(aaa);
+for (let i = 0; i < aaa.length; i++) {
+  aaa[i].style.opacity = 0;
+}
+const jk = () => {
+  for (let i = 0; i < aaa.length; i++) {
+    aaa[i].style.opacity += 0.1;
+  }
+}
+*/
