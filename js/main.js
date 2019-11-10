3339939d03c0cfe1201e404833f5e688709c9bb6
takeshisakuma <saude0518@gmail.com>
2019-11-10 20:34:40 +0900

    add loading

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
-"use strict";
-//# sourceMappingURL=main.js.map
+const loaderLayer = document.getElementById("js-loadingLayer");
+const loadedTransiton = () => {
+  loaderLayer.classList.remove("active");
+}
+window.addEventListener("load", loadedTransiton);
